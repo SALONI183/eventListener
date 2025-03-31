960d6de489764ddf118ef8e1996df466b3d9569b
Saloni sharma <156882921+saloni234sharma@users.noreply.github.com>
2025-04-01 04:02:31 +0530

    Removed deprecated MongoDB connection options

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,21 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-  
     const MONGO_URI = process.env.MONGO_URI;
     if (!MONGO_URI) {
       throw new Error("MONGO_URI is not defined in environment variables");
     }
 
-    
     console.log("Connecting to MongoDB with URI: ", MONGO_URI);
 
-    
+    // Remove the deprecated options
     await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
     });
 
     console.log("MongoDB connected successfully");
@@ -27,4 +23,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
