fc30d63ca169ba233c00d42fc34be7bfcaf7a18a
Saloni sharma <156882921+saloni234sharma@users.noreply.github.com>
2025-04-01 04:29:23 +0530

    Fixed MongoDB URI to use Atlas

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -9,11 +9,7 @@ const connectDB = async () => {
 
     console.log("Connecting to MongoDB with URI: ", MONGO_URI);
 
-    // Remove the deprecated options
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    
 
     console.log("MongoDB connected successfully");
   } catch (error) {
